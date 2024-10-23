f34980937e791d2e929d133dfcb02be7250c1d2f
Uvissh <vishalbhateria@gmail.com>
2024-10-23 20:02:07 +0530

    validdatorsandusing more schematype

--- src/models/user.js
+++ src/models/user.js
@@ -2,26 +2,60 @@
  const userSchema = mongoose.Schema({
 
     firstName:{
-    type:String
+    type:String,
+    required:true
+    
 },
 lastName:{
     type:String
 },
 emailId:{
-    type:String
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    lowercase:"true"
+    
 },
 password:{
-    type:Number
+    type:Number,
+    required:true,
+    min:8
+    
     
 },
 age:{
-    type:Number
+    type:Number,
+    min:18
 },
 gender:{
-    type:String
+    type:String,
+    validate(value){
+        if(!["male","female","other"].includes(value)){
+            throw new Error("Gender data is not valid")
+            
+
+        }
+    },
+
+},
+photoUrl:{
+    type:String,
+    default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s"
+},
+about:{
+    type:String,
+    default:"This is default about users"
+},
+skills:{
+    type:[String],
 
 }
 
- })
+ },
+ {
+    timestamps: true,
+ }
+)
  const User = mongoose.model("User",userSchema);//first is creating the name od model and the second is name of schema
  module.exports = User;
